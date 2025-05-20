e7c4e71b63f01cc3d92c3edc93296da61914b4ff
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-05-20 18:08:13 -0500

    feat: implement product pagination and enhance pagination UI

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,14 +1,33 @@
-import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+import { getPaginatedProductsWithImages } from "@/actions";
+import { Pagination, ProductGrid, Title } from "@/components";
+import { redirect } from "next/navigation";
 
-const products = initialData.products;
+interface Props {
+  searchParams: Promise<{ page?: string }>;
+}
 
-export default function Home() {
+export default async function Home({ searchParams }: Props) {
+  const searchParamsAwaited = await searchParams;
+  const page = searchParamsAwaited.page
+    ? parseInt(searchParamsAwaited.page)
+    : 1;
+
+  const { products, currentPage, totalPages } =
+    await getPaginatedProductsWithImages({ page });
+
+  console.log(currentPage, totalPages);
+
+  if (products.length === 0) {
+    redirect("/");
+  }
+
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
 
       <ProductGrid products={products} />
+
+      <Pagination totalPages={totalPages} />
     </>
   );
 }
