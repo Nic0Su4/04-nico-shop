12e0fbd5b71cc2a58baf4bda839a61bf651e1ff1
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-05-20 00:40:46 -0500

    chore: update moduleResolution in tsconfig.json to 'bundler'

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,12 @@
-import { PrismaClient } from "@/generated/prisma"; // Tu ruta de importación
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // Esto permite que TypeScript reconozca `prisma` en el objeto global.
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = globalThis.prisma ?? new PrismaClient();
+export const prisma = globalThis.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = prisma;
 }
-
-export { prisma };
--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -2,7 +2,49 @@ import { initialData } from "./seed";
 import { prisma } from "../lib/prisma";
 
 async function main() {
-  prisma.console.log("Seed ejecutado correctamente");
+  // 1. Borrar registros previos
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  // 2. Crear categorias
+  const { categories, products } = initialData;
+
+  const categoriesData = categories.map((name) => ({ name }));
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  // 3. Crear productos
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    // 4. Crear imagenes
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
+  });
+
+  console.log("Seed ejecutado correctamente");
 }
 
 (() => {
